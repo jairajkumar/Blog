7de4adf99504660eb9084641ebe41ddda498a9e4
Jairaj <Jairajkumar3105@gmail.com>
2020-06-17 04:41:11 +0530

    blog page created

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('blog', views.blog, name = 'blog'),
+    path('', views.PostList.as_view(), name = 'home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render,HttpResponse
+# from django.shortcuts import render,HttpResponse
 
-# Create your views here.
-def home(request):
-    return render(request, 'blog/blogindex.html')
+# # Create your views here.
+# def home(request):
+#     return render(request, 'blog/blogindex.html')
 
-def blog(request):
-    return HttpResponse('this is blog')
+# def blog(request):
+#     return HttpResponse('this is blog')
+
+
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog/blogs.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'blog/blog_detail.html'
